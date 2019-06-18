df8cf49787126754e69c34f99576af434fdad1c6
EL GOURCHAL <rajaa.el-gourchal@capgemini.com>
2019-06-18 12:20:00 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,167 @@
+var url = 'https://swapi.co/api/people/?format=json&page=1';
+var firstData = '';
+var paginationData = '';
+var b='';
+var tabtri='';
+var idmethode =0;
+
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+
+//Methode pour afficher les données dans tableau
+function afficher(tableau){
+    var size=tableau.length;
+    var output= "";
+    for(var i=0; i<size; i++){
+    
+        output += `
+        <tr>
+        <td> ${tableau[i].name}</td>
+        <td> ${tableau[i].height}</td>
+        <td> ${tableau[i].mass}</td>
+        <td> ${tableau[i].hair_color}</td>
+        <td> ${tableau[i].skin_color}</td>
+        <td> ${tableau[i].eye_color}</td>
+        <td> ${tableau[i].birth_year}</td>
+        </tr>	
+    `;
+    }
+
+    document.getElementById("nom").innerHTML= output;
+ } 
+
+//Methode de tri par nom
+function tri(a,b)
+{
+    return (a.name.toLowerCase() > b.name.toLowerCase()) ? 1:-1;
+}
+//Methode de tri par height
+function trinum(a,b)
+{
+    return (a.height - b.height);
+}
+function paginationcondition(){
+    if(paginationData.next) {
+        next.classList.remove("disbaled");
+        next.disabled = false;
+    }else {
+        next.classList.add("disbaled");
+        next.disabled = true;
+    }
+    
+    if(paginationData.previous) {
+        prev.classList.remove("disbaled");
+        prev.disabled = false;
+    } else {
+        prev.classList.add("disbaled");
+        prev.disabled = true;
+   }
+}
+function verificationid(){
+    if (idmethode==1)
+    {
+        //Si l'id egale a la valeur 1 , on implement la methode tri par nom
+        triparnom();
+    }
+    if (idmethode==2) {
+        //Si l'id egale a la valeur 2 , on implement la methode tri par height
+        triparheight();
+    }
+    if (idmethode==3) {
+        //Si l'id egale a la valeur 3 , on implement la methode recherche
+        recherche();
+       
+        console.log("test recherche");
+        console.log(t);
+    }
+}
+
+
+//Methode pour recupérer les données Json
+const getDatafinal = async (url) => {
+    let users = await fetch(url);
+    let data = await users.json();
+    //Data sur laquelle on vas appliquer la pagination
+    paginationData = data;
+    firstData = data.results;
+    console.log(paginationData);
+    setFirstdataaa(firstData);
+    paginationcondition();
+    verificationid();
+    afficher(t); 
+}
+
+//implementation de la methode de récupération des données
+getDatafinal(url);
+
+//Methode pour afficher tout les données dans un tableau avec pagination
+const affichertout =    () => {
+    console.log("Méthode pour afficher tout les données");
+    afficher(firstData);
+}
+
+
+//methode pour cloner l'objet retourner de la base de donnees pour ne pas modifier les donnees initiales
+const setFirstdataaa= async (x) =>{
+    t = Object.assign({},x);
+    //the type of fistdata is an object so we should convert to an array
+    //Converting an Object to an Array of Objects to apply the function Sort()
+    t = Object.keys(firstData).map(i => firstData[i]);
+}
+
+//Méthode pour afficher les données triées par nom
+const triparnom =  () => {
+    
+    tabtri=  t.sort(tri);
+    afficher(tabtri);
+    //l'id de la methode tri par nom =1
+    idmethode=1;
+
+}
+
+//methode pour afficher les données triées par Height
+const triparheight = () => {
+   
+   tabtri=  t.sort(trinum);
+    afficher(tabtri);
+    //l'id de la methode tri par Height=2
+    idmethode=2;
+
+}
+//methode pour afficher les personnes qui ont skin.color="fair"
+const recherche =  () => {
+    idmethode=3;
+       var output='';
+       for(var i =0;i<t.length;i++){
+        if(t[i].skin_color=="fair"){
+            output += `
+            <tr>
+            <td> ${t[i].name}</td>
+            <td> ${t[i].height}</td>
+            <td> ${t[i].mass}</td>
+            <td> ${t[i].hair_color}</td>
+            <td> ${t[i].skin_color}</td>
+            <td> ${t[i].eye_color}</td>
+            <td> ${t[i].birth_year}</td>
+            </tr>	
+        `;
+       }
+      
+       }
+       //on affecte la valeur 3 a l'id de la methode recherche
+      
+    document.getElementById("nom").innerHTML =output;
+   
+   }
+
+//methode pour pagination next
+function pageNext() {
+    console.log(paginationData.next);
+    getDatafinal(paginationData.next);
+    
+}
+//methode pour pagination previous
+function pagePrev() {
+    console.log(paginationData.previous);
+    getDatafinal(paginationData.previous);
+}
